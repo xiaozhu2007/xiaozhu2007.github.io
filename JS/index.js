5097e02d4700d87b37da209f8b70059c4d58eb77
xiaozhu2007 <63779340+xiaozhu2007@users.noreply.github.com>
2020-06-22 14:44:50 +0800

    n

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e4a6948b399d84c27ebfd93fed849d32";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
